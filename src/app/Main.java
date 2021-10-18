3f7c5a26383175510adebf82897af90f5cac95ef
Ruddy Monlouis <r.monlouis@gmail.com>
2021-10-18 10:57:30 +0200

    First commit

---
+++ src/app/Main.java
@@ -0,0 +1,14 @@
+package app;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
