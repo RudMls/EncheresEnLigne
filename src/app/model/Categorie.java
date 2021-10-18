b82d544a6bb58092bce4962dbf49e7fa4d782272
peicong he <acyeol1997@gmail.com>
2021-10-18 16:10:49 +0200

    categorie

--- src/app/model/Categorie.java
+++ src/app/model/Categorie.java
@@ -1,7 +1,20 @@
 package app.model;
 
 public class Categorie {
-
+    private long id;
     private String libelle;
+
+    public Categorie(long id, String libelle) {
+        this.id = id;
+        this.libelle = libelle;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
 
 }
